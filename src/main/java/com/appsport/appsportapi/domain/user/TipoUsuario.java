cca0e1bf073db3402b83e61403954586fd374034
nicol <nicollasmonteiro0800@gmail.com>
2025-04-27 18:17:14 -0300

    feat: Atualizações na Classe TipoUsuario

--- src/main/java/com/appsport/appsportapi/domain/user/TipoUsuario.java
+++ src/main/java/com/appsport/appsportapi/domain/user/TipoUsuario.java
@@ -1,11 +1,21 @@
 package com.appsport.appsportapi.domain.user;
 
 public enum TipoUsuario {
-    ADMIN,
-    USER,
-    ATHLETE,
-    MANAGER,
-    REFEREE,
-    SPECTATOR,
-    ORGANIZER,
+    ADMIN("admin"),
+    USER("usuario");
+    ;
+//    ATHLETE,
+//    MANAGER,
+//    REFEREE,
+//    SPECTATOR,
+//    ORGANIZER,
+    private String tipo;
+
+    TipoUsuario(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
 }
