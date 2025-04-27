5e1b19b00f72bd852e817ea9bb3b78e64201722b
nicol <nicollasmonteiro0800@gmail.com>
2025-04-27 18:24:46 -0300

    feat: Att UsuarioRepository FindByEmail

--- src/main/java/com/appsport/appsportapi/repositories/UsuarioRepository.java
+++ src/main/java/com/appsport/appsportapi/repositories/UsuarioRepository.java
@@ -2,10 +2,13 @@ package com.appsport.appsportapi.repositories;
 
 import com.appsport.appsportapi.domain.user.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    UserDetails findUsuarioByEmail(String email);
     Optional<Usuario> findUsuarioByCpf(String cpf);
     Optional<Usuario> findUsuarioById(Long id);
 }
