73bc9010474d557ae211ca3a1d0507037f7141e3
nicol <nicollasmonteiro0800@gmail.com>
2025-04-28 23:04:58 -0300

    feat: Novo método para inserir novos Usuários

--- src/main/java/com/appsport/appsportapi/domain/user/Usuario.java
+++ src/main/java/com/appsport/appsportapi/domain/user/Usuario.java
@@ -31,6 +31,12 @@ public class Usuario implements UserDetails {
     @Enumerated(EnumType.STRING)
     private TipoUsuario tipoUsuario;
 
+    public Usuario(String email, String senha, TipoUsuario tipoUsuario) {
+        this.email = email;
+        this.senha = senha;
+        this.tipoUsuario = tipoUsuario;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         if(this.tipoUsuario == TipoUsuario.ADMIN)
