6638cbff7e33adc2447c11b8f4dbb029f82c645f
nicol <nicollasmonteiro0800@gmail.com>
2025-04-28 23:06:41 -0300

    feat: Controller para codificar senha

---
+++ src/main/java/com/appsport/appsportapi/controllers/AutenticacaoController.java
@@ -0,0 +1,46 @@
+package com.appsport.appsportapi.controllers;
+
+import com.appsport.appsportapi.domain.user.AutenticacaoDTO;
+import com.appsport.appsportapi.domain.user.RegistrarDTO;
+import com.appsport.appsportapi.domain.user.Usuario;
+import com.appsport.appsportapi.repositories.UsuarioRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("autenticacao")
+public class AutenticacaoController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+    @PostMapping("/email")
+    public ResponseEntity login(@RequestBody @Valid AutenticacaoDTO data) {
+        var usernamePassword = new UsernamePasswordAuthenticationToken(data.email(), data.senha());
+        var auth = this.authenticationManager.authenticate(usernamePassword);
+
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/registrar")
+    public ResponseEntity registrar(@RequestBody @Valid RegistrarDTO data) {
+        if(this.usuarioRepository.findUsuarioByEmail(data.email())!= null) return ResponseEntity.badRequest().build();  {
+        }
+        String encryptedPassword = new BCryptPasswordEncoder().encode(data.senha());
+        Usuario newUsuario = new Usuario(data.email(), encryptedPassword, data.tipoUsuario());
+
+        this.usuarioRepository.save(newUsuario);
+
+        return ResponseEntity.ok().build();
+    }
+}
