87a920077590b717cea6a0c665ae44545075762f
nicol <nicollasmonteiro0800@gmail.com>
2025-05-03 16:27:03 -0300

    feat: Criação da Entidade Eventos

---
+++ src/main/java/com/appsport/appsportapi/domain/eventos/Eventos.java
@@ -0,0 +1,36 @@
+package com.appsport.appsportapi.domain.eventos;
+
+import com.appsport.appsportapi.domain.categoria.Categoria;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Date;
+
+@Table(name = "qbdf_eventos")
+@Entity(name = "Eventos")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Eventos {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(name = "id_categoria", referencedColumnName = "id")
+    private Categoria categoria;
+
+    private Date data_evento;
+
+    private String local;
+
+    private String descricao;
+
+    private String url_evento;
+
+    private String organizador;
+}
