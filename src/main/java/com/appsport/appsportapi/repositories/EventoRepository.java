45626eb0b816e48c6d704ee7d3f588f700112333
nicol <nicollasmonteiro0800@gmail.com>
2025-05-04 15:37:55 -0300

    feat: Criação da EventoRepository

---
+++ src/main/java/com/appsport/appsportapi/repositories/EventoRepository.java
@@ -0,0 +1,7 @@
+package com.appsport.appsportapi.repositories;
+
+import com.appsport.appsportapi.domain.eventos.Eventos;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EventoRepository extends JpaRepository<Eventos, String> {
+}
