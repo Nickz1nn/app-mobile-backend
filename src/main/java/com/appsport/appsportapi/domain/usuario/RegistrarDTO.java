b67c2d2b30a5591bc2520c7e78f677806b309f19
nicol <nicollasmonteiro0800@gmail.com>
2025-05-05 23:27:17 -0300

    feat: Adição das demais variáveis

--- src/main/java/com/appsport/appsportapi/domain/usuario/RegistrarDTO.java
+++ src/main/java/com/appsport/appsportapi/domain/usuario/RegistrarDTO.java
@@ -1,5 +1,5 @@
 package com.appsport.appsportapi.domain.usuario;
 
-public record RegistrarDTO(String email, String senha, TipoUsuario tipoUsuario) {
+public record RegistrarDTO(String primeiroNome, String sobrenome, String cpf,String email, String senha, TipoUsuario tipoUsuario) {
 
 }
