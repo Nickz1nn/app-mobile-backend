b9eb057880e00e5feae2c58bb17d2881f79257bc
nicol <nicollasmonteiro0800@gmail.com>
2025-05-07 20:32:55 -0300

    feat: Criação da CategoriaService

---
+++ src/main/java/com/appsport/appsportapi/services/CategoriaService.java
@@ -0,0 +1,19 @@
+package com.appsport.appsportapi.services;
+
+import com.appsport.appsportapi.domain.categoria.Categoria;
+import com.appsport.appsportapi.domain.categoria.CategoriaRequestDTO;
+import com.appsport.appsportapi.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoriaService {
+    @Autowired
+    private CategoriaRepository categoriaRepository;
+
+    public Categoria salvarCategoria(CategoriaRequestDTO data) {
+        Categoria categoria = new Categoria();
+        categoria.setNome(data.nome());
+        return categoriaRepository.save(categoria);
+    }
+}
