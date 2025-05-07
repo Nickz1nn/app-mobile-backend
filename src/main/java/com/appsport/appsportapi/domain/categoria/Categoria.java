9b1b312b5a93d075e52a27b849d99bdfea04b62a
nicol <nicollasmonteiro0800@gmail.com>
2025-05-06 23:51:26 -0300

    feat: Atualizações na entidade Categoria

--- src/main/java/com/appsport/appsportapi/domain/categoria/Categoria.java
+++ src/main/java/com/appsport/appsportapi/domain/categoria/Categoria.java
@@ -5,18 +5,20 @@ import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Table (name = "qbdf_categoria")
-@Entity (name = "Categoria")
+@Table(name = "qbdf_categoria")
+@Entity(name = "Categoria")
 @Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@EqualsAndHashCode (of = "id")
-
+@EqualsAndHashCode(of = "id")
 public class Categoria {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false, unique = true)
     private String nome;
 }
