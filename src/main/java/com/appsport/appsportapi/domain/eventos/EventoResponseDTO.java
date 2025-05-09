93e233bc6e06ef6bbd5898bc0f2207ccb9351d69
nicol <nicollasmonteiro0800@gmail.com>
2025-05-09 18:57:58 -0300

    feat: Att na EventoResponseDTO

--- src/main/java/com/appsport/appsportapi/domain/eventos/EventoResponseDTO.java
+++ src/main/java/com/appsport/appsportapi/domain/eventos/EventoResponseDTO.java
@@ -5,7 +5,13 @@ import java.util.Date;
 public record EventoResponseDTO(String id, String nome, String categoria, Date data, String local,
                                 String descricao, String url_evento, String organizador) {
     public EventoResponseDTO(Eventos eventos) {
-        this(eventos.getId(), eventos.getNome(), eventos.getCategoria().getNome(), eventos.getData_evento(),
-                eventos.getLocal(), eventos.getDescricao(), eventos.getUrl_evento(), eventos.getOrganizador());
+        this(eventos.getId(),
+                eventos.getNome(),
+                eventos.getCategoria() != null ? eventos.getCategoria().getNome() : null,
+                eventos.getData_evento(),
+                eventos.getLocal(),
+                eventos.getDescricao(),
+                eventos.getUrl_evento(),
+                eventos.getOrganizador());
     }
 }
