67bf53cf0279d892cb33f5a9b3ebf4e42ddbb578
nicol <nicollasmonteiro0800@gmail.com>
2025-05-09 18:57:07 -0300

    feat: Alteração na EventoController para listar e criar os Eventos

--- src/main/java/com/appsport/appsportapi/controllers/EventoController.java
+++ src/main/java/com/appsport/appsportapi/controllers/EventoController.java
@@ -4,6 +4,7 @@ import com.appsport.appsportapi.domain.eventos.EventoRequestDTO;
 import com.appsport.appsportapi.domain.eventos.EventoResponseDTO;
 import com.appsport.appsportapi.domain.eventos.Eventos;
 import com.appsport.appsportapi.repositories.EventoRepository;
+import com.appsport.appsportapi.services.EventoService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,23 +18,29 @@ public class EventoController {
 
     @Autowired
     EventoRepository eventoRepository;
+    @Autowired
+    private EventoService eventoService;
 
     @PostMapping
-    public ResponseEntity postEvento(@RequestBody @Valid EventoRequestDTO body){
-        Eventos newEvento = new Eventos(body);
-
-        this.eventoRepository.save(newEvento);
-        return ResponseEntity.ok().build();
+    public ResponseEntity criarEvento(@RequestBody @Valid EventoRequestDTO body){
+        Eventos evento = eventoService.criarEvento(body);
+        return ResponseEntity.ok(new EventoResponseDTO(evento));
     }
 
     @GetMapping
-    public ResponseEntity getAllEventos(){
-        List<EventoResponseDTO> eventosList = this.eventoRepository
-                .findAll()
-                .stream()
-                .map(EventoResponseDTO::new)
-                .toList();
 
-        return ResponseEntity.ok(eventosList);
+    public ResponseEntity<List<EventoResponseDTO>> listarEventos(){
+        var eventos = eventoService.listarEventos();
+        return ResponseEntity.ok(eventos);
     }
+
+//    public ResponseEntity getAllEventos(){
+//        List<EventoResponseDTO> eventosList = this.eventoRepository
+//                .findAll()
+//                .stream()
+//                .map(EventoResponseDTO::new)
+//                .toList();
+//
+//        return ResponseEntity.ok(eventosList);
+//    }
 }
