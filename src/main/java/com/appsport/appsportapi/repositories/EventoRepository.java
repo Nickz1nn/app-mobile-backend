1a47c41ef9883af0317dbf27b8a7d65f605cdc44
nicol <nicollasmonteiro0800@gmail.com>
2025-05-12 23:25:05 -0300

    feat: Alteração na Estretégia UUID para ID e adição dos métodos para Deletar e Buscar por ID.

--- src/main/java/com/appsport/appsportapi/repositories/EventoRepository.java
+++ src/main/java/com/appsport/appsportapi/repositories/EventoRepository.java
@@ -3,5 +3,5 @@ package com.appsport.appsportapi.repositories;
 import com.appsport.appsportapi.domain.eventos.Eventos;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EventoRepository extends JpaRepository<Eventos, String> {
+public interface EventoRepository extends JpaRepository<Eventos, Long> {
 }
